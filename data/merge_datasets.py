1f45c0e20b659aad711c2b25a8712aef152bb7dc
ashishodu2023 <ashish.odu.2023@gmail.com>
2025-03-02 11:57:13 -0800

    Updated the run pipeline

--- data/merge_datasets.py
+++ data/merge_datasets.py
@@ -17,7 +17,7 @@ class MergeDatasets:
         from data_utils import get_traces  # example
         traces, timestamps, flags, files = [], [], [], []
 
-        for dcml in file_list[:250]:
+        for dcml in file_list[:100]:
             if alarm is not None:
                 tmp_trace, tmp_timestamp = get_traces(
                     dcml, var_id="Trace2", begin=3000,
